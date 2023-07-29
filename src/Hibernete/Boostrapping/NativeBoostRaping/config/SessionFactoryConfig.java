b43fedbd0a60c7cb153fb1f94456a05610becc90
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-07-29 14:21:36 +0530

    Session Factory Config

---
+++ src/Hibernete/Boostrapping/NativeBoostRaping/config/SessionFactoryConfig.java
@@ -0,0 +1,41 @@
+package Hibernete.Boostrapping.NativeBoostRaping.config;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactoryConfig {
+
+    private static SessionFactoryConfig factoryConfig;
+
+    private SessionFactoryConfig(){}
+
+    public static SessionFactoryConfig getFactoryConfig(){
+//        if(null== factoryConfig){
+//            return factoryConfig =new SessionFactoryConfig();
+//        }
+//        return factoryConfig;
+        return (null == factoryConfig)
+                ? factoryConfig= new SessionFactoryConfig() : factoryConfig;
+    }
+
+    public Session getSession(){
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        Metadata metadata =  new MetadataSources(serviceRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+        SessionFactory sessionFactory = metadata.buildSessionFactory();
+
+        return sessionFactory.openSession();
+
+
+
+    }
+}
