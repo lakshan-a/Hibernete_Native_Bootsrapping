a986bdf41fc538fcd60f7b9950b6a4b9645277ec
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-04 21:52:09 +0530

    private static SessionFactory factory;

--- src/Hibernete/Boostrapping/NativeBoostRaping/config/SessionFactoryConfigToProperty.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/config/SessionFactoryConfigToProperty.java
@@ -1,4 +1,28 @@
 package Hibernete.Boostrapping.NativeBoostRaping.config;
 
+import Hibernete.Boostrapping.NativeBoostRaping.entity.Customer;
+import Hibernete.Boostrapping.NativeBoostRaping.entity.Item;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class SessionFactoryConfigToProperty {
+    private static SessionFactory factory;
+    private static SessionFactoryConfigToProperty configToProperty;
+
+    private SessionFactoryConfigToProperty(){
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        try {
+            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.Properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        configuration.addAnnotatedClass(Customer.class).addAnnotatedClass(Item.class);
+        factory=configuration.setProperties(properties).buildSessionFactory();
+    }
 }
