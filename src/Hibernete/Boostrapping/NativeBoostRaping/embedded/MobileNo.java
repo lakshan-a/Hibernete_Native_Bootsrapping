649100c75f2f10fac165ccd3bce821512b2fce84
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-05 16:52:41 +0530

    Mobile No coding

--- src/Hibernete/Boostrapping/NativeBoostRaping/embedded/MobileNo.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/embedded/MobileNo.java
@@ -1,4 +1,40 @@
 package Hibernete.Boostrapping.NativeBoostRaping.embedded;
 
 public class MobileNo {
+
+    private String type;
+    private String mobileLine;
+
+    public MobileNo (){
+
+    }
+
+    public MobileNo(String type, String mobileLine) {
+        this.type = type;
+        this.mobileLine = mobileLine;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMobileLine() {
+        return mobileLine;
+    }
+
+    public void setMobileLine(String mobileLine) {
+        this.mobileLine = mobileLine;
+    }
+
+    @Override
+    public String toString() {
+        return "MobileNo{" +
+                "type='" + type + '\'' +
+                ", mobileLine='" + mobileLine + '\'' +
+                '}';
+    }
 }
