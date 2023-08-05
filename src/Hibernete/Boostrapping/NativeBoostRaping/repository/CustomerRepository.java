971b5fbd69f227c6314306a03a41a2538f4fcd15
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-05 13:59:04 +0530

    CustomerRepository coding

--- src/Hibernete/Boostrapping/NativeBoostRaping/repository/CustomerRepository.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/repository/CustomerRepository.java
@@ -1,4 +1,18 @@
 package Hibernete.Boostrapping.NativeBoostRaping.repository;
 
+import Hibernete.Boostrapping.NativeBoostRaping.config.SessionFactoryConfig;
+import Hibernete.Boostrapping.NativeBoostRaping.entity.Customer;
+import org.hibernate.Session;
+
 public class CustomerRepository {
+
+    private final Session session;
+
+    public CustomerRepository(){
+       session = SessionFactoryConfig.getInstance().getSession();
+    }
+
+    public Customer getCustomer(int id){
+        return session.get(Customer.class,1);
+    }
 }
