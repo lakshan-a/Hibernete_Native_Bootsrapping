fa6bbad5fd0bf4d629bedd39afd28cf5f4039aa6
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-05 16:37:50 +0530

    NameIdentifire coding

--- src/Hibernete/Boostrapping/NativeBoostRaping/embedded/NameIdentifire.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/embedded/NameIdentifire.java
@@ -1,4 +1,49 @@
 package Hibernete.Boostrapping.NativeBoostRaping.embedded;
 
 public class NameIdentifire {
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    public NameIdentifire(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "NameIdentifire{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
