f7b55f42ff6cedd59b17b33d7574b4dd614bf68c
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-05 15:17:50 +0530

    Customer try catch

--- src/Hibernete/Boostrapping/NativeBoostRaping/repository/CustomerRepository.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/repository/CustomerRepository.java
@@ -16,10 +16,18 @@ public class CustomerRepository {
     public int saveCustomer(Customer customer){
         Transaction transaction = session.beginTransaction();
 
-        int customerId = (int) session.save(customer);
-        transaction.commit();
-        session.close();
-        return customerId;
+        try {
+            int customerId = (int) session.save(customer);
+            transaction.commit();
+            session.close();
+            return customerId;
+        }catch (Exception  e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return -1;
+        }
+
     }
 
     public Customer getCustomer(int id){
