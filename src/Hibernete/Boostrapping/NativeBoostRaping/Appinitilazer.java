63ae1af97876fa751755e20d8c234629167a0717
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-05 08:45:40 +0530

    cteate delete code

--- src/Hibernete/Boostrapping/NativeBoostRaping/Appinitilazer.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/Appinitilazer.java
@@ -24,21 +24,30 @@ public class Appinitilazer {
 //        session.close();
 
         //update
-        Session session1 = SessionFactoryConfig.getInstance().getSession();
-        Transaction transaction1 = session1.beginTransaction();
-         Customer customer = session1.get(Customer.class,1);
-                 customer.setAddress("Matara");
-                 customer.setName("Rashmiks");
-                 session1.update(customer);
-                 transaction1.commit();
-                 session1.close();
+//        Session session1 = SessionFactoryConfig.getInstance().getSession();
+//        Transaction transaction1 = session1.beginTransaction();
+//         Customer customer = session1.get(Customer.class,1);
+//                 customer.setAddress("Matara");
+//                 customer.setName("Rashmiks");
+//                 session1.update(customer);
+//                 transaction1.commit();
+//                 session1.close();
+//
+//         Session session2 = SessionFactoryConfig.getInstance().getSession();
+//         Transaction transaction = session2.beginTransaction();
+//         Customer customer1 = session2.get(Customer.class,1);
+//         session2.delete(customer1);
+//         transaction.commit();
+//         session2.close();
 
-         Session session2 = SessionFactoryConfig.getInstance().getSession();
-         Transaction transaction = session2.beginTransaction();
-         Customer customer1 = session2.get(Customer.class,1);
-         session2.delete(customer1);
-         transaction.commit();
-         session2.close();
+        //3. delete
+
+        Session deleteSession = SessionFactoryConfig.getInstance().getSession();
+        Transaction deleteTransaction = deleteSession.beginTransaction();
+        Customer deleteCustomer = deleteSession.get(Customer.class,1);
+        deleteSession.delete(deleteCustomer);
+        deleteTransaction.commit();
+        deleteSession.close();
 
 
 
