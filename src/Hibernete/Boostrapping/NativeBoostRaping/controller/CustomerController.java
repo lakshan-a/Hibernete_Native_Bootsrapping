a65ab7a410551b91db561fc906e364e11fd3e3ae
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-09 19:40:36 +0530

    create view

--- src/Hibernete/Boostrapping/NativeBoostRaping/controller/CustomerController.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/controller/CustomerController.java
@@ -1,56 +1,105 @@
 package Hibernete.Boostrapping.NativeBoostRaping.controller;
 
-import Hibernete.Boostrapping.NativeBoostRaping.entity.Customer;
-import Hibernete.Boostrapping.NativeBoostRaping.repository.CustomerRepository;
-import com.sun.deploy.config.Config;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class CustomerController {
-    @FXML
-    private Button upBtn;
-    @FXML
-    private Button dltBtn;
-    @FXML
-    private TextField adTxt;
-
 
     @FXML
-    private Button scBtn;
-    @FXML
-    private TextField idTxt;
+    private Button btnDelete;
 
     @FXML
-    private TextField nmTxt;
+    private Button btnSave;
 
     @FXML
-    private TextField salTxt;
+    private Button btnSearch;
 
     @FXML
-    void svBtnOnAction(ActionEvent event) {
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colAd;
+
+    @FXML
+    private TableColumn<?, ?> colFirstName;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colLastName;
+
+    @FXML
+    private TableColumn<?, ?> colMiddleName;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TextField cstAd;
+
+    @FXML
+    private TextField cstAd1;
+
+    @FXML
+    private TextField cstId;
+
+    @FXML
+    private TextField cstName;
+
+    @FXML
+    private TextField cstName1;
+
+    @FXML
+    private TextField cstName2;
+
+    @FXML
+    private TableView<?> cstTbl;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
 
     }
+
     @FXML
-    void scBtnOnAction(ActionEvent event) {
-
-
+    void btnSaveOnAction(ActionEvent event) {
 
     }
 
-
     @FXML
-    void upBtnOnAction(ActionEvent event) {
-
-
+    void btnSearchOnAction(ActionEvent event) {
 
     }
+
     @FXML
-    void dltBtnOnAction(ActionEvent event) {
+    void btnUpdateOnAction(ActionEvent event) {
 
+    }
 
+    public void btnBack(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/Hibernete/Boostrapping/NativeBoostRaping/view/Home.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        Platform.runLater(() -> primaryStage.sizeToScene());
     }
 }
--- src/Hibernete/Boostrapping/NativeBoostRaping/controller/ItemController.java
+++ src/Hibernete/Boostrapping/NativeBoostRaping/controller/ItemController.java
@@ -1,13 +1,22 @@
 package Hibernete.Boostrapping.NativeBoostRaping.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ItemController {
 
@@ -76,4 +85,14 @@ public class ItemController {
 
     }
 
+    public void btnBack(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/Hibernete/Boostrapping/NativeBoostRaping/view/Home.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        Platform.runLater(() -> primaryStage.sizeToScene());
+
+    }
 }
